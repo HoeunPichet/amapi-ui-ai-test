7bfbd2fd5ab0914705b28716df9570a2be8d20a6
pichet <good100vichet@gmail.com>
2025-10-11 01:01:32 +0700

    test vercel

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://auth.amapi.site/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
